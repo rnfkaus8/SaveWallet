290ded4f69690395da23daa3203e7f208ec76111
rnfkaus8 <kth4350@gmail.com>
2023-06-04 22:45:31 +0900

    add model index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createRealmContext } from '@realm/react';
 import Home from './screens/home/Home';
-import { Item } from './model/Item';
+import { RealmContext } from './model';
 
 const Stack = createNativeStackNavigator();
-const defaultPathLocalRealm = createRealmContext({ schema: [Item] });
-const { RealmProvider } = defaultPathLocalRealm;
+const { RealmProvider } = RealmContext;
 const App = () => {
   return (
     <NavigationContainer>
