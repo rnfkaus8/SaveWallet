12cf2460828f0c8d26d44a666e44416f18653a79
rnfkaus8 <kth4350@gmail.com>
2023-06-04 21:10:45 +0900

    add react native navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/home/Home';
 
+const Stack = createNativeStackNavigator();
 const App = () => {
-  return <SafeAreaView />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
