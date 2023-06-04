12458707d954d6d642130bac7bf2fa6838200cbd
rnfkaus8 <kth4350@gmail.com>
2023-06-04 22:14:22 +0900

    add RealmProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createRealmContext } from '@realm/react';
 import Home from './screens/home/Home';
+import { Item } from './model/Item';
 
 const Stack = createNativeStackNavigator();
+const defaultPathLocalRealm = createRealmContext({ schema: [Item] });
+const { RealmProvider } = defaultPathLocalRealm;
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+      <RealmProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </RealmProvider>
     </NavigationContainer>
   );
 };
