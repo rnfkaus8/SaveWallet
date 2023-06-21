d1d4f6b3773f1bbb0a9c7c13dbd6bda3592483a6
rnfkaus8 <kth4350@gmail.com>
2023-06-21 20:20:57 +0900

    카테고리 제거

--- src/model/Item.ts
+++ src/model/Item.ts
@@ -1,16 +1,11 @@
 import Realm from 'realm';
 
-export const CategoryArr = ['save', 'waste'];
-
-export type Category = (typeof CategoryArr)[number];
 export class Item extends Realm.Object<Item> {
   _id: Realm.BSON.ObjectId;
 
   name: string;
 
   price: number;
-
-  category: Category;
 
   date: Date;
 
@@ -20,7 +15,6 @@ export class Item extends Realm.Object<Item> {
       _id: 'objectId',
       name: 'string',
       price: 'int',
-      category: 'string',
       date: 'date',
     },
     primaryKey: '_id',
