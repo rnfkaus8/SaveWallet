43f9e6d5dcd451aa9a03e273bb6f865d4b6f60e9
rnfkaus8 <kth4350@gmail.com>
2023-06-29 21:48:40 +0900

    사용하지 않는 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Home from './screens/home/Home';
-import HomeTableItemForm from './screens/home/HomeTableItemForm';
 import { RouteName } from './assets/navigation';
 import RealmContext from './model';
-import HomeTableItemUpdateForm from './screens/home/HomeTableItemUpdateForm';
 
 const Stack = createNativeStackNavigator();
 const { RealmProvider } = RealmContext;
@@ -17,14 +15,6 @@ const App = () => {
         <RealmProvider>
           <Stack.Navigator>
             <Stack.Screen name={RouteName.Home} component={Home} />
-            <Stack.Screen
-              name={RouteName.HomeTableItemForm}
-              component={HomeTableItemForm}
-            />
-            <Stack.Screen
-              name={RouteName.HomeTableItemUpdateForm}
-              component={HomeTableItemUpdateForm}
-            />
           </Stack.Navigator>
         </RealmProvider>
       </NavigationContainer>
