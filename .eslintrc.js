af92dbb9194ad4548ce264dceb8a32875ee67301
rnfkaus8 <kth4350@gmail.com>
2023-06-29 21:48:29 +0900

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,7 +72,7 @@ module.exports = {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'warn',
     '@typescript-eslint/no-unsafe-call' : 'off',
-    'eslint-disable-next-line no-underscore-dangle':'off'
+    'eslint-disable-next-line':'off', 'no-underscore-dangle':'off'
   },
   settings: {
     react: {
