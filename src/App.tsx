b79f6f5fd8b2b90e84e25acd40a2728bbb94b870
rnfkaus8 <kth4350@gmail.com>
2023-07-17 20:28:43 +0900

    add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Provider } from 'react-redux';
 import Home from './screens/home/Home';
 import { RouteName } from './assets/navigation';
 import RealmContext from './model';
 import useMemberUpdate from './hooks/useMemberUpdate';
 import { useGoalInitialize } from './hooks/useGoalInitialize';
+import { store } from './store';
 
 const Stack = createNativeStackNavigator();
 const { RealmProvider } = RealmContext;
 const App = () => {
-  useMemberUpdate();
-  useGoalInitialize();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <RealmProvider>
-          <Stack.Navigator>
-            <Stack.Screen name={RouteName.Home} component={Home} />
-          </Stack.Navigator>
-        </RealmProvider>
-      </NavigationContainer>
+      <Provider store={store}>
+        <NavigationContainer>
+          <RealmProvider>
+            <Stack.Navigator>
+              <Stack.Screen name={RouteName.Home} component={Home} />
+            </Stack.Navigator>
+          </RealmProvider>
+        </NavigationContainer>
+      </Provider>
     </GestureHandlerRootView>
   );
 };
