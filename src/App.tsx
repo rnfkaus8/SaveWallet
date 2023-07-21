649f7c9a70057e9c87ad1ee1af893e5ab696b80b
rnfkaus8 <kth4350@gmail.com>
2023-07-21 19:53:43 +0900

    사용하지 않는 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
