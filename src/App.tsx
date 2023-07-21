28d454c08653343783d19e24d08ac1f145a19aa3
rnfkaus8 <kth4350@gmail.com>
2023-07-21 19:27:15 +0900

    add RNBootSplash.hide, ref : https://github.com/zoontek/react-native-bootsplash#with-react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
+import RNBootSplash from 'react-native-bootsplash';
 import Home from './screens/home/Home';
 import { RouteName } from './assets/navigation';
 import RealmContext from './model';
-import useMemberUpdate from './hooks/useMemberUpdate';
-import { useGoalInitialize } from './hooks/useGoalInitialize';
 import { store } from './store';
 
 const Stack = createNativeStackNavigator();
@@ -16,7 +15,11 @@ const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Provider store={store}>
-        <NavigationContainer>
+        <NavigationContainer
+          onReady={() => {
+            RNBootSplash.hide();
+          }}
+        >
           <RealmProvider>
             <Stack.Navigator>
               <Stack.Screen name={RouteName.Home} component={Home} />
