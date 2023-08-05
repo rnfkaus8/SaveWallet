69e29282bacb351f6a10b53b97517caba57000eb
rnfkaus8 <kth4350@gmail.com>
2023-08-05 18:26:24 +0900

    member 모델 변경에 따른 state 변경

--- src/states/memberState.ts
+++ src/states/memberState.ts
@@ -1,15 +1,17 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
-import Realm from 'realm';
-import { Member } from '../model/Member';
 
 export interface MemberState {
-  id: string;
-  startedAt: string;
+  id: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const initialState: MemberState = {
-  id: '',
-  startedAt: new Date().toISOString(),
+  id: 0,
+  name: '',
+  createdAt: new Date(),
+  updatedAt: new Date(),
 };
 
 export const memberFetched = createAction<MemberState>('member/memberFetched');
@@ -17,6 +19,8 @@ export const memberFetched = createAction<MemberState>('member/memberFetched');
 export const memberReducer = createReducer(initialState, (builder) => {
   builder.addCase(memberFetched, (state, action) => {
     state.id = action.payload.id;
-    state.startedAt = action.payload.startedAt;
+    state.name = action.payload.name;
+    state.createdAt = action.payload.createdAt;
+    state.updatedAt = action.payload.updatedAt;
   });
 });
