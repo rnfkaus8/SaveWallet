aa35242fd94e4a976004a34a5fcf8d2e668042c9
rnfkaus8 <kth4350@gmail.com>
2023-08-05 18:24:22 +0900

    사용하지 않는 코드 제거

--- src/model/index.ts
+++
@@ -1,8 +0,0 @@
-import { createRealmContext } from '@realm/react';
-import { Item } from './Item';
-import { Goal } from './Goal';
-import { Member } from './Member';
-
-const RealmContext = createRealmContext({ schema: [Item, Goal, Member] });
-
-export default RealmContext;
