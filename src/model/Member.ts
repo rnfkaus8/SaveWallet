691cd655424b3ce56cae8982f06a1f2c9a2b9c9c
rnfkaus8 <kth4350@gmail.com>
2023-08-18 21:18:17 +0900

    member serialize deserialize 추가

--- src/model/Member.ts
+++ src/model/Member.ts
@@ -1,3 +1,12 @@
+import { safeParseJSON } from '../common/SafeParseJson';
+
+export interface SerializeMemberProps {
+  id: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 export class Member {
   private _id: number;
 
@@ -28,5 +37,27 @@ export class Member {
     this._name = name;
     this._createdAt = createdAt;
     this._updatedAt = updatedAt;
+  }
+
+  static deserialize(data: string): Member {
+    const serializeMember = safeParseJSON<SerializeMemberProps>(data);
+
+    return new Member(
+      serializeMember!.id,
+      serializeMember!.name,
+      serializeMember!.createdAt,
+      serializeMember!.updatedAt,
+    );
+  }
+
+  public serialize(): string {
+    const data: SerializeMemberProps = {
+      id: this._id,
+      name: this._name,
+      createdAt: this._createdAt,
+      updatedAt: this._updatedAt,
+    };
+
+    return JSON.stringify(data);
   }
 }
