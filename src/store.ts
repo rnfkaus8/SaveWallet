e2853e60cc77a6930172492954739782c82a94e3
rnfkaus8 <kth4350@gmail.com>
2023-08-18 21:20:11 +0900

    인증 redux store 추가

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { memberReducer } from './states/memberState';
 import { goalReducer } from './states/goalState';
+import { authReducer } from './states/authState';
 
 export const store = configureStore({
   reducer: {
     member: memberReducer,
     goal: goalReducer,
+    auth: authReducer,
   },
 });
 
