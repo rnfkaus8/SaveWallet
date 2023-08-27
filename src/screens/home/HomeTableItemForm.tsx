4cc482524e2ce1717c3d2342c587fcd62275e170
rnfkaus8 <kth4350@gmail.com>
2023-08-27 19:01:56 +0900

    구매 목록 추가 모달 디자인 수정

--- src/screens/home/HomeTableItemForm.tsx
+++ src/screens/home/HomeTableItemForm.tsx
@@ -1,9 +1,17 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Text, TextInput, TouchableOpacity } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import DatePicker from 'react-native-date-picker';
 import styled from 'styled-components/native';
 import Modal from 'react-native-modal';
+import moment from 'moment';
 import { itemRepository } from '../../repository';
+import { VerticalSpacer } from '../../common/components/VerticalSpacer';
 
 interface HomeTableItemFormProps {
   onPressSubmitItem(): void;
@@ -16,19 +24,29 @@ const Wrapper = styled.View`
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
   background-color: white;
-  padding: 20px;
+  padding: 32px 20px;
 `;
 
 const InputWrapper = styled.View`
-  margin-bottom: 10px;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+`;
+
+const DateWrapper = styled.View`
+  flex-direction: row;
+  display: flex;
+  align-items: flex-start;
+  align-self: stretch;
 `;
 
 const InputTitle = styled.Text`
-  text-align: left;
-  justify-content: center;
-  align-items: center;
-  margin-right: 10px;
-  margin-bottom: 10px;
+  color: #222;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24; /* 24px */
 `;
 
 const HomeTableItemForm: React.FC<HomeTableItemFormProps> = ({
@@ -74,66 +92,120 @@ const HomeTableItemForm: React.FC<HomeTableItemFormProps> = ({
       useNativeDriver
       swipeDirection="down"
     >
-      <Wrapper>
-        <InputWrapper>
-          <InputTitle>상품명</InputTitle>
-          <TextInput
-            style={{
-              padding: 10,
-              borderRadius: 10,
-              borderWidth: 2,
-              borderColor: '#000',
-            }}
-            onChangeText={setName}
-            value={name}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <InputTitle>가격</InputTitle>
-          <TextInput
-            style={{
-              padding: 10,
-              borderRadius: 10,
-              borderWidth: 2,
-              borderColor: '#000',
-            }}
-            keyboardType="number-pad"
-            onChangeText={handleChangePrice}
-            value={priceStr}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <TouchableOpacity onPress={handlePressDatePicker}>
-            <InputTitle>날짜</InputTitle>
-          </TouchableOpacity>
-          <DatePicker
-            modal
-            date={date}
-            open={datePrickerOpen}
-            mode="date"
-            onConfirm={(confirmedDate) => {
-              setDatePickerOpen(false);
-              setDate(confirmedDate);
-            }}
-            onCancel={() => {
-              setDatePickerOpen(false);
-            }}
-          />
-          <InputTitle>{date.toLocaleDateString()}</InputTitle>
-        </InputWrapper>
+      <KeyboardAvoidingView behavior="padding">
+        <Wrapper>
+          <InputWrapper>
+            <InputTitle>구매한 물건</InputTitle>
+            <VerticalSpacer size={12} />
+            <View
+              style={{
+                width: '100%',
+                padding: 16,
+                borderRadius: 8,
+                borderColor: '#F4F4F4',
+              }}
+            >
+              <TextInput
+                style={{
+                  fontSize: 16,
+                  fontFamily: 'Pretendard',
+                  fontStyle: 'normal',
+                  fontWeight: '400',
+                  lineHeight: 24,
+                }}
+                onChangeText={setName}
+                placeholder="구매한 물건 이름을 적으세요."
+                value={name}
+              />
+            </View>
+          </InputWrapper>
+          <VerticalSpacer size={24} />
+          <InputWrapper>
+            <InputTitle>소비 금액</InputTitle>
+            <VerticalSpacer size={12} />
+            <View
+              style={{
+                width: '100%',
+                padding: 16,
+                borderRadius: 8,
+                borderColor: '#F4F4F4',
+              }}
+            >
+              <TextInput
+                style={{
+                  fontSize: 16,
+                  fontFamily: 'Pretendard',
+                  fontStyle: 'normal',
+                  fontWeight: '400',
+                  lineHeight: 24,
+                }}
+                placeholder="소비 금액을 적으세요"
+                keyboardType="number-pad"
+                onChangeText={handleChangePrice}
+                value={priceStr}
+              />
+            </View>
+          </InputWrapper>
+          <VerticalSpacer size={24} />
+          <DateWrapper>
+            <InputTitle style={{ flex: 1 }}>날짜</InputTitle>
+            <DatePicker
+              modal
+              date={date}
+              open={datePrickerOpen}
+              mode="date"
+              onConfirm={(confirmedDate) => {
+                setDatePickerOpen(false);
+                setDate(confirmedDate);
+              }}
+              onCancel={() => {
+                setDatePickerOpen(false);
+              }}
+            />
+            <TouchableOpacity
+              style={{ flex: 1 }}
+              onPress={handlePressDatePicker}
+            >
+              <Text
+                style={{
+                  color: '#888',
+                  fontFamily: 'Pretendard',
+                  fontSize: 16,
+                  fontStyle: 'normal',
+                  fontWeight: '400',
+                  lineHeight: 24,
+                  textAlign: 'right',
+                }}
+              >
+                {moment(date).format('YYYY년 MM월 DD일')}
+              </Text>
+            </TouchableOpacity>
+          </DateWrapper>
+        </Wrapper>
         <TouchableOpacity
           style={{
             width: '100%',
             height: 50,
-            backgroundColor: 'yellow',
+            backgroundColor: '#878787',
             justifyContent: 'center',
             alignItems: 'center',
           }}
           onPress={handlePressSubmit}
         >
-          <Text>Submit!!</Text>
+          <Text
+            style={{
+              color: '#FFF',
+              textAlign: 'center',
+              fontFamily: 'Pretendard',
+              fontSize: 16,
+              fontStyle: 'normal',
+              fontWeight: '700',
+            }}
+          >
+            저장
+          </Text>
         </TouchableOpacity>
-      </Wrapper>
+      </KeyboardAvoidingView>
     </Modal>
   );
 };
