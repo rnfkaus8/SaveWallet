dc7147642f9b1019772fb9b2cfbe3fb10c540aeb
rnfkaus8 <kth4350@gmail.com>
2023-09-06 13:45:05 +0900

    svg 파일에 style props 추가

--- src/common/svg/SvgProps.ts
+++ src/common/svg/SvgProps.ts
@@ -1,5 +1,8 @@
+import { StyleProp, ViewStyle } from 'react-native';
+
 export interface SvgProps {
   width: number;
   height: number;
   fillColor: string;
+  style?: StyleProp<ViewStyle>;
 }
--- src/common/svg/UpArrow.tsx
+++ src/common/svg/UpArrow.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
 import { SvgProps } from './SvgProps';
 
-const UpArrow = ({ width, height, fillColor }: SvgProps) => {
+const UpArrow = ({ width, height, fillColor, style }: SvgProps) => {
   return (
     <Svg
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
+      style={style}
       fill={fillColor}
     >
       <Path
