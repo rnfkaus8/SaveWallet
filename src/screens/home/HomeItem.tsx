0b16b952b62c7ff077ce67707fff92bebc9c1d95
rnfkaus8 <kth4350@gmail.com>
2023-09-09 17:30:53 +0900

    소비 목록 탭 화면 적용 (더보기 화면 개발 예정)

--- src/screens/home/HomeItem.tsx
+++ src/screens/home/HomeItem.tsx
@@ -8,23 +8,21 @@ import {
 } from 'react-native';
 import React, { useCallback, useMemo } from 'react';
 import styled from 'styled-components/native';
+import moment from 'moment';
 import { Item } from '../../model/Item';
 import { VerticalSpacer } from '../../common/components/VerticalSpacer';
 import { edit, trashcan } from '../../assets/resources/images';
 import { itemRepository } from '../../repository';
-
-const ListWrapper = styled.View`
-  flex: 1;
-  padding: 24px 20px;
-`;
+import MoreIcon from '../../common/svg/MoreIcon';
 
 const RowWrapper = styled.TouchableOpacity`
-  border-radius: 10px;
-  border-width: 2px;
-  border-color: #000;
-  padding: 10px;
-  margin-bottom: 15px;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+  align-self: stretch;
   position: relative;
+  border-radius: 20px;
+  background: #f9f9f9;
 `;
 
 const Row = styled.View`
@@ -32,15 +30,23 @@ const Row = styled.View`
   flex-direction: row;
 `;
 
+const ItemInfo = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  flex: 1;
+`;
+
 const ToggleWrapper = styled.View`
+  flex-direction: column;
+  border-radius: 8px;
+  background: #fff;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.1);
+  padding: 16px;
   position: absolute;
   top: 20px;
   right: 20px;
-  background-color: white;
-  border-color: black;
-  border-width: 1px;
-  border-radius: 5px;
-  padding: 10px;
 `;
 
 interface HomeItemProps {
@@ -95,14 +101,46 @@ export const HomeItem: React.FC<HomeItemProps> = ({
           }}
         >
           <Row>
-            <Text style={{ flex: 1, textAlign: 'left' }}>{item.name}</Text>
-            <Text style={{ flex: 1, textAlign: 'right' }}>{item.price}</Text>
-          </Row>
-          <VerticalSpacer size={10} />
-          <Row>
-            <Text style={{ flex: 1, textAlign: 'right' }}>
-              {itemCreatedDateStr}
+            <ItemInfo>
+              <Text
+                style={{
+                  color: '#000',
+                  fontFamily: 'Pretendard',
+                  fontSize: 16,
+                  fontStyle: 'normal',
+                  fontWeight: '500',
+                  lineHeight: 24.0,
+                }}
+              >
+                {item.name}
+              </Text>
+              <Text
+                style={{
+                  color: '#757575',
+                  fontFamily: 'Pretendard',
+                  fontSize: 14,
+                  fontStyle: 'normal',
+                  fontWeight: '400',
+                  lineHeight: 21.0,
+                }}
+              >
+                {moment(item.boughtDate).format('M월 D일')}
+              </Text>
+            </ItemInfo>
+            <VerticalSpacer size={12} />
+            <Text
+              style={{
+                color: '#121212',
+                fontFamily: 'Pretendard',
+                fontSize: 16,
+                fontStyle: 'normal',
+                fontWeight: '700',
+                lineHeight: 24.0,
+              }}
+            >
+              {item.price.toLocaleString()}원
             </Text>
+            <MoreIcon width={20} height={20} fillColor="#121212" />
           </Row>
           <ToggleWrapper
             style={{
