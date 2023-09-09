a003360270d9b47278998ab57835eec60d1ab2f3
rnfkaus8 <kth4350@gmail.com>
2023-09-09 17:30:23 +0900

    더보기 아이콘 추가

---
+++ src/common/svg/MoreIcon.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Svg, { Circle } from 'react-native-svg';
+import { SvgProps } from './SvgProps';
+
+const MoreIcon = ({ width, height, fillColor }: SvgProps) => {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill={fillColor}
+    >
+      <Circle
+        cx={10}
+        cy={5.083}
+        r={1.25}
+        fill="#121212"
+        transform="rotate(90 10 5.083)"
+      />
+      <Circle
+        cx={10}
+        cy={10.5}
+        r={1.25}
+        fill="#121212"
+        transform="rotate(90 10 10.5)"
+      />
+      <Circle
+        cx={10}
+        cy={15.917}
+        r={1.25}
+        fill="#121212"
+        transform="rotate(90 10 15.917)"
+      />
+    </Svg>
+  );
+};
+export default MoreIcon;
