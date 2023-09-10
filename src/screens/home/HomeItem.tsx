1cfaec6b3999aa1a1c24aad09178afd2ed4c2243
rnfkaus8 <kth4350@gmail.com>
2023-09-10 19:16:35 +0900

    소비 목록 화면 개발

--- src/screens/home/HomeItem.tsx
+++ src/screens/home/HomeItem.tsx
@@ -4,6 +4,7 @@ import {
   ListRenderItemInfo,
   Text,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import React, { useCallback, useMemo } from 'react';
@@ -15,12 +16,11 @@ import { edit, trashcan } from '../../assets/resources/images';
 import { itemRepository } from '../../repository';
 import MoreIcon from '../../common/svg/MoreIcon';
 
-const RowWrapper = styled.TouchableOpacity`
+const RowWrapper = styled.View`
   padding: 20px;
   justify-content: center;
   align-items: center;
   align-self: stretch;
-  position: relative;
   border-radius: 20px;
   background: #f9f9f9;
 `;
@@ -39,14 +39,14 @@ const ItemInfo = styled.View`
 `;
 
 const ToggleWrapper = styled.View`
-  flex-direction: column;
   border-radius: 8px;
   background: #fff;
-  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.1);
   padding: 16px;
   position: absolute;
-  top: 20px;
+  top: 7px;
   right: 20px;
+  width: 140px;
+  z-index: 999;
 `;
 
 interface HomeItemProps {
@@ -54,7 +54,7 @@ interface HomeItemProps {
   onPressItemEditModalOpen: () => void;
   onPressItemRow: (isSelectedItem: boolean, item: Item) => void;
   selectedItem: Item | null;
-  onPressItemDelete: (itemId: number) => Promise<void>;
+  onPressItemDelete: (itemId: number) => void;
 }
 
 export const HomeItem: React.FC<HomeItemProps> = ({
@@ -64,107 +64,116 @@ export const HomeItem: React.FC<HomeItemProps> = ({
   selectedItem,
   onPressItemDelete,
 }) => {
-  const listEmptyComponent = useMemo(() => {
-    return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text
-          style={{
-            textAlign: 'right',
-            color: '#555',
-            fontFamily: 'Pretendard',
-            fontSize: 15,
-            fontStyle: 'normal',
-            fontWeight: '500',
-            lineHeight: 22.5,
-          }}
-        >
-          아직 등록 된 소비 목록이 없어요.
-        </Text>
-      </View>
-    );
-  }, []);
-
   const renderItem = useCallback(
     ({ item }: ListRenderItemInfo<Item>) => {
       const isSelectedItem = selectedItem?.id.toString() === item.id.toString();
-      const itemCreatedDateStr = new Date(item.boughtDate).toLocaleDateString();
       return (
-        <RowWrapper
-          onPress={() => {
-            onPressItemRow(isSelectedItem, item);
-          }}
-        >
-          <Row>
-            <ItemInfo>
-              <Text
-                style={{
-                  color: '#000',
-                  fontFamily: 'Pretendard',
-                  fontSize: 16,
-                  fontStyle: 'normal',
-                  fontWeight: '500',
-                  lineHeight: 24.0,
-                }}
-              >
-                {item.name}
-              </Text>
-              <Text
-                style={{
-                  color: '#757575',
-                  fontFamily: 'Pretendard',
-                  fontSize: 14,
-                  fontStyle: 'normal',
-                  fontWeight: '400',
-                  lineHeight: 21.0,
-                }}
-              >
-                {moment(item.boughtDate).format('M월 D일')}
-              </Text>
-            </ItemInfo>
-            <VerticalSpacer size={12} />
-            <Text
-              style={{
-                color: '#121212',
-                fontFamily: 'Pretendard',
-                fontSize: 16,
-                fontStyle: 'normal',
-                fontWeight: '700',
-                lineHeight: 24.0,
-              }}
-            >
-              {item.price.toLocaleString()}원
-            </Text>
-            <MoreIcon width={20} height={20} fillColor="#121212" />
-          </Row>
-          <ToggleWrapper
-            style={{
-              display: isSelectedItem ? 'flex' : 'none',
+        <View style={{ flex: 1 }}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              onPressItemRow(isSelectedItem, item);
             }}
           >
-            <TouchableOpacity
-              style={{ flexDirection: 'row' }}
-              onPress={() => {
-                onPressItemDelete(item.id);
+            <RowWrapper>
+              <Row>
+                <ItemInfo>
+                  <Text
+                    style={{
+                      color: '#000',
+                      fontFamily: 'Pretendard',
+                      fontSize: 16,
+                      fontStyle: 'normal',
+                      fontWeight: '500',
+                      lineHeight: 24.0,
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                  <Text
+                    style={{
+                      color: '#757575',
+                      fontFamily: 'Pretendard',
+                      fontSize: 14,
+                      fontStyle: 'normal',
+                      fontWeight: '400',
+                      lineHeight: 21.0,
+                    }}
+                  >
+                    {moment(item.boughtDate).format('M월 D일')}
+                  </Text>
+                </ItemInfo>
+                <VerticalSpacer size={12} />
+                <View style={{ justifyContent: 'center' }}>
+                  <Text
+                    style={{
+                      color: '#121212',
+                      fontFamily: 'Pretendard',
+                      fontSize: 16,
+                      fontStyle: 'normal',
+                      fontWeight: '700',
+                      lineHeight: 24.0,
+                    }}
+                  >
+                    {item.price.toLocaleString()}원
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  style={{ justifyContent: 'center' }}
+                  onPress={() => {
+                    onPressItemRow(isSelectedItem, item);
+                  }}
+                >
+                  <MoreIcon width={20} height={20} fillColor="#121212" />
+                </TouchableOpacity>
+              </Row>
+            </RowWrapper>
+          </TouchableWithoutFeedback>
+          {isSelectedItem && (
+            <ToggleWrapper
+              style={{
+                shadowColor: '#000',
+                shadowOpacity: 0.1,
+                shadowOffset: { width: 0, height: 4 },
+                shadowRadius: 20,
+                elevation: 5,
               }}
             >
-              <Image source={trashcan} style={{ width: 14, height: 14 }} />
-              <Text>삭제하기</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{ flexDirection: 'row' }}
-              onPress={onPressItemEditModalOpen}
-            >
-              <Image source={edit} style={{ width: 14, height: 14 }} />
-              <Text>수정하기</Text>
-            </TouchableOpacity>
-          </ToggleWrapper>
-        </RowWrapper>
+              <TouchableOpacity
+                onPress={() => {
+                  onPressItemDelete(item.id);
+                }}
+              >
+                <Text
+                  style={{
+                    color: '#000',
+                    fontFamily: 'Pretendard',
+                    fontSize: 15,
+                    fontStyle: 'normal',
+                    fontWeight: '400',
+                    lineHeight: 22.5,
+                  }}
+                >
+                  삭제하기
+                </Text>
+              </TouchableOpacity>
+              <VerticalSpacer size={8} />
+              <TouchableOpacity onPress={onPressItemEditModalOpen}>
+                <Text
+                  style={{
+                    color: '#000',
+                    fontFamily: 'Pretendard',
+                    fontSize: 15,
+                    fontStyle: 'normal',
+                    fontWeight: '400',
+                    lineHeight: 22.5,
+                  }}
+                >
+                  수정하기
+                </Text>
+              </TouchableOpacity>
+            </ToggleWrapper>
+          )}
+        </View>
       );
     },
     [
@@ -174,5 +183,15 @@ export const HomeItem: React.FC<HomeItemProps> = ({
       selectedItem?.id,
     ],
   );
-  return <FlatList data={items} renderItem={renderItem} />;
+  return (
+    <FlatList
+      style={{ padding: 20 }}
+      data={items}
+      renderItem={renderItem}
+      removeClippedSubviews={false}
+      ItemSeparatorComponent={() => {
+        return <VerticalSpacer size={12} />;
+      }}
+    />
+  );
 };
