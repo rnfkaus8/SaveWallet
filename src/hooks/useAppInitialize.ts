f7496a16751340d73a2d5f94e9e8c93b504d48df
rnfkaus8 <kth4350@gmail.com>
2023-09-28 15:25:54 +0900

    deviceId로 로그인이 자동으로 되게 수정

--- src/hooks/useAppInitialize.ts
+++ src/hooks/useAppInitialize.ts
@@ -1,26 +1,51 @@
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import DeviceInfo from 'react-native-device-info';
 import { Member } from '../model/Member';
 import { memberFetched } from '../states/memberState';
 import { authInitialize } from '../states/authState';
+import { memberRepository } from '../repository';
 
 export const useAppInitialize = () => {
   const dispatch = useDispatch();
+
+  const init = useCallback(async () => {
+    const serializeMember = await AsyncStorage.getItem('userInfo');
+    if (serializeMember) {
+      const member = Member.deserialize(serializeMember);
+      dispatch(
+        memberFetched({
+          id: member.id,
+          name: member.name,
+          updatedAt: member.updatedAt,
+          createdAt: member.createdAt,
+        }),
+      );
+    } else {
+      const deviceId = await DeviceInfo.getUniqueId();
+      const memberProps = await memberRepository.login(deviceId);
+      const member = new Member(
+        memberProps.id,
+        memberProps.name,
+        memberProps.createdAt,
+        memberProps.updatedAt,
+      );
+      dispatch(
+        memberFetched({
+          id: member.id,
+          name: member.name,
+          updatedAt: member.updatedAt,
+          createdAt: member.createdAt,
+        }),
+      );
+
+      await AsyncStorage.setItem('userInfo', member.serialize());
+    }
+    dispatch(authInitialize());
+  }, [dispatch]);
+
   useEffect(() => {
-    AsyncStorage.getItem('userInfo').then((data) => {
-      if (data) {
-        const member = Member.deserialize(data);
-        dispatch(
-          memberFetched({
-            id: member.id,
-            name: member.name,
-            updatedAt: member.updatedAt,
-            createdAt: member.createdAt,
-          }),
-        );
-        dispatch(authInitialize());
-      }
-    });
-  });
+    init();
+  }, [init]);
 };
