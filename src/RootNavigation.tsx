24bbb5bc672bb8cbaa0e3736c3bc3abc2c596073
rnfkaus8 <kth4350@gmail.com>
2023-09-29 13:01:26 +0900

    자동 로그인이 되기 때문에 로그인 페이지는 제거한다

--- src/RootNavigation.tsx
+++ src/RootNavigation.tsx
@@ -1,23 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import RNBootSplash from 'react-native-bootsplash';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useDispatch, useSelector } from 'react-redux';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { RouteName } from './assets/navigation';
-import { Login } from './screens/member/Login';
 import Home from './screens/home/Home';
-import { Member } from './model/Member';
-import { memberFetched } from './states/memberState';
-import { RootState } from './store';
 import { useAppInitialize } from './hooks/useAppInitialize';
 
 export const RootNavigation = () => {
   useAppInitialize();
-
-  const isAuthInitialize = useSelector<RootState, boolean>((state) => {
-    return state.auth.isAuthInitialize;
-  });
 
   const handleNavigationContainerReady = useCallback(() => {
     RNBootSplash.hide();
@@ -26,9 +16,6 @@ export const RootNavigation = () => {
   return (
     <NavigationContainer onReady={handleNavigationContainerReady}>
       <Stack.Navigator>
-        {!isAuthInitialize && (
-          <Stack.Screen name={RouteName.Login} component={Login} />
-        )}
         <Stack.Screen
           options={{ headerShown: false }}
           name={RouteName.Home}
