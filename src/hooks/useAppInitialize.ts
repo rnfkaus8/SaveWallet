e59171567a8cb1cdb51a98bd80e5aff2d203e4e3
rnfkaus8 <kth4350@gmail.com>
2023-09-30 14:16:22 +0900

    소소한 리팩토링

--- src/hooks/useAppInitialize.ts
+++ src/hooks/useAppInitialize.ts
@@ -10,39 +10,37 @@ import { memberRepository } from '../repository';
 export const useAppInitialize = () => {
   const dispatch = useDispatch();
 
+  const getMember = async (serializeMember: string | null) => {
+    if (serializeMember) {
+      return Member.deserialize(serializeMember);
+    }
+
+    const deviceId = await DeviceInfo.getUniqueId();
+    const memberProps = await memberRepository.login(deviceId);
+
+    return new Member(
+      memberProps.id,
+      memberProps.name,
+      memberProps.createdAt,
+      memberProps.updatedAt,
+    );
+  };
+
   const init = useCallback(async () => {
     const serializeMember = await AsyncStorage.getItem('userInfo');
-    if (serializeMember) {
-      const member = Member.deserialize(serializeMember);
-      dispatch(
-        memberFetched({
-          id: member.id,
-          name: member.name,
-          updatedAt: member.updatedAt,
-          createdAt: member.createdAt,
-        }),
-      );
-    } else {
-      const deviceId = await DeviceInfo.getUniqueId();
-      const memberProps = await memberRepository.login(deviceId);
-      const member = new Member(
-        memberProps.id,
-        memberProps.name,
-        memberProps.createdAt,
-        memberProps.updatedAt,
-      );
-      dispatch(
-        memberFetched({
-          id: member.id,
-          name: member.name,
-          updatedAt: member.updatedAt,
-          createdAt: member.createdAt,
-        }),
-      );
 
-      await AsyncStorage.setItem('userInfo', member.serialize());
-    }
-    dispatch(authInitialize());
+    const member = await getMember(serializeMember);
+
+    dispatch(
+      memberFetched({
+        id: member.id,
+        name: member.name,
+        updatedAt: member.updatedAt,
+        createdAt: member.createdAt,
+      }),
+    );
+
+    await AsyncStorage.setItem('userInfo', member.serialize());
   }, [dispatch]);
 
   useEffect(() => {
