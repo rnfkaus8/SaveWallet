60f5eefdbdcbc080289eb812476d41626ba206d2
rnfkaus8 <kth4350@gmail.com>
2023-10-01 16:38:05 +0900

    사용하지 않는 코드 제거

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { memberReducer } from './states/memberState';
 import { goalReducer } from './states/goalState';
-import { authReducer } from './states/authState';
 
 export const store = configureStore({
   reducer: {
     member: memberReducer,
     goal: goalReducer,
-    auth: authReducer,
   },
 });
 
