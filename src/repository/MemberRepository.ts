91a50cd12d8069111833c2b0b7c0ed076fdf8520
rnfkaus8 <kth4350@gmail.com>
2023-11-15 19:12:16 +0900

    멤버 에러핸들링

--- src/repository/MemberRepository.ts
+++ src/repository/MemberRepository.ts
@@ -1,17 +1,24 @@
 import axios from 'axios';
 import Config from 'react-native-config';
 import { Member, SerializeMemberProps } from '../model/Member';
+import { ErrorResponse } from '../model/ErrorResponse';
 
 export default class MemberRepository {
-  login = async (name: string): Promise<SerializeMemberProps> => {
-    const axiosResponse = await axios.get<SerializeMemberProps>(
-      `${Config.API_URL}/member/${name}`,
-    );
+  login = async (
+    name: string,
+  ): Promise<SerializeMemberProps | ErrorResponse> => {
+    try {
+      const { data } = await axios.get<SerializeMemberProps>(
+        `${Config.API_URL}/member/${name}`,
+      );
+      return data;
+    } catch (e) {
+      if (axios.isAxiosError<ErrorResponse>(e) && e.response) {
+        const { code, message } = e.response.data;
+        return new ErrorResponse(code, message);
+      }
 
-    if (axiosResponse.status === 200) {
-      return axiosResponse.data;
+      return new ErrorResponse('500', '서버 에러입니다.');
     }
-
-    throw Error('존재하지 않는 회원입니다');
   };
 }
